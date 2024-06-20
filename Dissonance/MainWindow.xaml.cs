05529b13fd143e5f600b99bce61e873a5c6d8c40
AdrienneBosch <adrienne.bosch7@gmail.com>
2024-06-20 22:17:41 +0200

    Removed the debugging textbox as settings are correctly loaded.

--- Dissonance/MainWindow.xaml.cs
+++ Dissonance/MainWindow.xaml.cs
@@ -16,38 +16,19 @@ namespace Dissonance
 			_settingsManager = settingsManager;
 			_appSettings = _settingsManager.LoadSettings ( );
 			InitializeSettings ( );
-			DisplaySettingsForDebugging ( );
 		}
 
 		private void InitializeSettings ( )
 		{
-			// Example of accessing settings
 			var volume = _appSettings.ScreenReader.Volume;
 			var voiceRate = _appSettings.ScreenReader.VoiceRate;
 			var zoomLevel = _appSettings.Magnifier.ZoomLevel;
 			var invertColors = _appSettings.Magnifier.InvertColors;
-
-			// Apply these settings to your application as needed
 		}
 
 		private void SaveSettings ( )
 		{
-			// Modify _appSettings as needed
 			_settingsManager.SaveSettings ( _appSettings );
-		}
-
-		private void DisplaySettingsForDebugging ( )
-		{
-			var settingsInfo = new StringBuilder();
-			settingsInfo.AppendLine ( "Screen Reader Settings:" );
-			settingsInfo.AppendLine ( $"Volume: {_appSettings.ScreenReader.Volume}" );
-			settingsInfo.AppendLine ( $"Voice Rate: {_appSettings.ScreenReader.VoiceRate}" );
-			settingsInfo.AppendLine ( );
-			settingsInfo.AppendLine ( "Magnifier Settings:" );
-			settingsInfo.AppendLine ( $"Zoom Level: {_appSettings.Magnifier.ZoomLevel}" );
-			settingsInfo.AppendLine ( $"Invert Colors: {_appSettings.Magnifier.InvertColors}" );
-
-			DebugTextBlock.Text = settingsInfo.ToString ( );
 		}
 	}
 }

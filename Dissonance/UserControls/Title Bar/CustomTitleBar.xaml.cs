5bfae7cf3069c2e844e521a120f1d211105bc8ca
AdrienneBosch <adrienne.bosch7@gmail.com>
2024-06-21 20:05:59 +0200

    Added a custom title bar. Looks bad.

---
+++ Dissonance/UserControls/Title Bar/CustomTitleBar.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dissonance.UserControls.Title_Bar
+{
+    /// <summary>
+    /// Interaction logic for CustomTitleBar.xaml
+    /// </summary>
+    public partial class CustomTitleBar : UserControl
+    {
+        public CustomTitleBar()
+        {
+            InitializeComponent();
+        }
+
+		private void MinimizeButton_Click ( object sender, RoutedEventArgs e )
+		{
+			Window.GetWindow ( this ).WindowState = WindowState.Minimized;
+		}
+
+		private void CloseButton_Click ( object sender, RoutedEventArgs e )
+		{
+			Window.GetWindow ( this ).Close ( );
+		}
+	}
+}

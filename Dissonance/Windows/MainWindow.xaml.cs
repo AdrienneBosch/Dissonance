17da32a8da4b53152848ac15152223d5fa7d9651
AdrienneBosch <adrienne.bosch7@gmail.com>
2024-06-29 18:15:14 +0200

    removed plane text toggle button

--- Dissonance/Windows/MainWindow.xaml.cs
+++ Dissonance/Windows/MainWindow.xaml.cs
@@ -17,14 +17,12 @@ namespace Dissonance
 			_appSettings = _settingsManager.LoadSettings ( );
 			InitializeSettings ( );
 
-			// Create instance of ThemeToggleButton
 			var themeToggleButton = new Dissonance.UserControls.Buttons.ThemeToggleButton
 			{
 				SettingsManager = _settingsManager,
 				AppSettings = _appSettings
 			};
 
-			// Add ThemeToggleButton to the container
 			ThemeToggleButtonContainer.Children.Add ( themeToggleButton );
 		}
 
@@ -35,29 +33,13 @@ namespace Dissonance
 			var zoomLevel = _appSettings.Magnifier.ZoomLevel;
 			var invertColors = _appSettings.Magnifier.InvertColors;
 
-			// Initialize the theme based on saved settings
 			bool isDarkMode = _appSettings.Theme.IsDarkMode;
-			ThemeToggleButton.IsChecked = isDarkMode;
 			ThemeManager.SetTheme ( isDarkMode );
 		}
 
 		private void SaveSettings ( )
 		{
 			_settingsManager.SaveSettings ( _appSettings );
-		}
-
-		private void ThemeToggleButton_Checked ( object sender, RoutedEventArgs e )
-		{
-			_appSettings.Theme.IsDarkMode = true; // Set dark mode
-			ThemeManager.SetTheme ( true ); // Apply dark mode
-			SaveSettings ( ); // Save settings
-		}
-
-		private void ThemeToggleButton_Unchecked ( object sender, RoutedEventArgs e )
-		{
-			_appSettings.Theme.IsDarkMode = true;
-			ThemeManager.SetTheme ( false );
-			SaveSettings ( );
 		}
 	}
 }
